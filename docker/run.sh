0195c498d9c9b7e1fe724d510424afc95e3393ad
Julien Fischer <jufi2112@gmail.com>
2022-05-27 15:32:41 +0200

    rewrote dockerfile to pytorch 1.11.0 and removed some currently unnecessary lines, part of #10

--- docker/run.sh
+++ docker/run.sh
@@ -14,7 +14,8 @@ else
 	BASE_DATA_DIR=/home/$USER/data/
 fi
 
-docker run --rm --gpus all --shm-size=8gb --name $USER-garmentor -it \
+docker run -it --rm --runtime=nvidia --gpus all --shm-size=8gb \
 	-v ${REPO_DIR}:/garmentor \
 	-v ${BASE_DATA_DIR}/3dpw/:/data/3dpw/ \
-	-v ${BASE_DATA_DIR}:/SSP-3D/ $USER-garmentor
+	-v ${BASE_DATA_DIR}:/SSP-3D/ \
+	$USER-garmentor
