cc10028835a75c130566e29bec351a3b0f054606
Julien Fischer <jufi2112@gmail.com>
2022-11-14 02:11:34 +0100

    working on scene generation script

--- data/const.py
+++ data/const.py
@@ -14,7 +14,7 @@ SCANS_DIR = os.path.join(AGORA_DIR, 'smplx_gt/')
 MGN_DATASET = os.path.join(MGN_DIR, 'Multi-Garment_dataset/')
 UV_MAPS_PATH = os.path.join(MGN_DIR, 'uv_maps/')
 SUBJECT_OBJ_SAVEDIR = os.path.join(GARMENTOR_DIR, 'agora/subjects/')
-SCENE_OBJ_SAVEDIR = os.path.join(GARMENTOR_DIR, 'agora/scenes/')
+SCENE_OBJ_SAVEDIR = os.path.join(GARMENTOR_DIR, 'agora', 'scenes')
 CAM_DIR = os.path.join(AGORA_DIR, 'camera/')
 
 TRAIN_CAM_DIR = os.path.join(CAM_DIR, 'train/')
--- data/generate_scene_meshes.py
+++ data/generate_scene_meshes.py
@@ -1,9 +1,15 @@
 import os
+from os import path as osp
 import argparse
 import numpy as np
 from itertools import compress
 import pickle5
 import sys
+import pandas as pd
+import random
+from typing import Tuple
+from tqdm import tqdm
+from random import randrange
 
 sys.path.append('/garmentor/')
 
@@ -15,18 +21,149 @@ from data.const import (
     TRAIN_CAM_DIR
 )
 
+def _sample_scene_data(
+    path_cam_files: str,
+    scene_name: str,
+    elements_to_sample: int = -1
+    ) -> Tuple[pd.Dataframe, int]:
+    """Samples the given amount of scene descriptions from the provided files.
+    If more elements are requested than exist, the maximum number of elements
+    is returned.
+    Args:
+        path_cam_files (str): Path to the directory that contains all camera
+            files.
+        scene_name (str): Name of the scene for which scene data should be
+            sampled
+        elements_to_sample (int, Optional): Maximum amount of elements that
+            should be sampled. If a value <= 0 is provided, samples the maximum
+            amount of elements possible. Defaults to -1
+    Returns:
+        pd.DataFrame: The sampled scene information
+        int: Number of sampled elements
+    """
+    next_index = 0
+    df_scene = pd.DataFrame()
+    for pkl_file in [file for file in os.listdir(path_cam_files) \
+        if osp.splitext(file)[1] == '.pkl']:
+        pkl_fpath = osp.join(path_cam_files, 'pkl_file')
+        df = pd.read_pickle(pkl_fpath)
+        # Unify indices among all cam files
+        number_elements = len(df.index)
+        df.index = np.arange(next_index, number_elements + next_index)
+        next_index = number_elements + next_index
+        # Only get the rows which contain entries for the specified scene
+        df_scene = pd.concat([
+            df_scene,
+            df[df['imgPath'].str.contains(scene_name)]
+        ])
+    # df_scene now contains all entries for the given scene
+    number_rows = len(df_scene.index)
+    elements_to_sample = min(elements_to_sample, number_rows) \
+        if elements_to_sample > 0 else number_rows
+    sample_indices = random.sample(
+        np.arange(number_rows).tolist(),
+        elements_to_sample
+    )
+    df_return = df_scene.iloc[sample_indices]
+    return df_return, elements_to_sample
+
+
+def _select_garment(garment_combination_occurences: np.ndarray):
+    """Selects a garment combination based on the already selected garments
+    with the goal of evenly selecting all garments.
+    Args:
+        garment_combination_occurences (np.ndarray): Array that, for each
+            garment combination, holds the number of times this garment
+            combination has already been selected.
+    Returns:
+        int: The index of the garment that should be selected, based on the
+            input array.
+    """
+    least_garment_occurences = np.where(
+        garment_combination_occurences == garment_combination_occurences.min()
+    )[0]
+    return least_garment_occurences[
+        randrange(least_garment_occurences.shape[0])]
+
+
+def generate_scene_samples(
+    subjects_base_dir: str,
+    scene_name: str,
+    scenes_to_generate: int = -1
+    ):
+    df, number_samples = _sample_scene_data(
+        subjects_base_dir,
+        scene_name,
+        scenes_to_generate
+    )
+
+    # make sure that each garment combination is represented evenly in the
+    # dataset
+    garment_combinations = [
+        element for element in os.listdir(SUBJECT_OBJ_SAVEDIR) \
+            if osp.isdir(osp.join(SUBJECT_OBJ_SAVEDIR, element))
+    ]
+    num_garment_combinations = len(garment_combinations)
+    gar_comb_occurences = np.zeros(num_garment_combinations, np.int32)
+
+    for idx in tqdm(range(number_samples), desc="Generating scenes"):
+        # Generate scene as given by the data
+        scene = df.iloc[idx]
+        kids = scene['kid']
+        adults = [not kid for kid in kids]
+
+        # select only the subjects that are adults
+        if np.asarray(kids).all():
+            print(f"Skipping index {idx} due to only containing children")
+            continue
+        Xs = list(compress(scene['X'], adults))
+        Ys = list(compress(scene['Y'], adults))
+        Zs = list(compress(scene['Z'], adults))
+
+        gt_paths_smplx = list(compress(scene['gt_path_smplx'], adults))
+        img_path = scene['imgPath']
+
+        camX = scene['camX']
+        camY = scene['camY']
+        camZ = scene['camZ']
+        camYaw = scene['camYaw']
+
+        output_dirpath = osp.join(
+            SCENE_OBJ_SAVEDIR,
+            scene_name,
+            osp.splitext(img_path)[0]
+        )
+
+        for subject_idx, gt_path_smplx in enumerate(gt_paths_smplx):
+            # Select garment combination
+            selected_garment_combination = _select_garment(gar_comb_occurences)
+            # this index can be e.g. used into garment_combinations to get the name
+
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--part_idx', '-I', type=int,
-                        help='Part index specifying PKL file index for camera information.')
-    parser.add_argument('--scene_idx', '-S', type=int,
-                        help='Scene index inside the specified part.')
+    parser.add_argument('--part-idx', '-I', type=int,
+        help='Part index specifying PKL file index for camera information.')
+    parser.add_argument('--scene-idx', '-S', type=int,
+        help='Scene index inside the specified part.')
+
+    parser.add_argument('-i', '--input', type=str, help="Path to the directory"
+        " that contains the .pkl files with scene informations.")
+    parser.add_argument('-s', '--scene-name', type=str,
+        help="Name of the scene for which scenes should be generated. E.g. '"
+        "archivz' or 'brushifygrasslands'. Has to be part of the 'imgPath' "
+        "attribute inside of the provided cam .pkl files.")
+    parser.add_argument('-n', '--number-scenes', type=int,
+        help="Maximum number of scenes that should be generated. If less data "
+        "is available, fewer scenes are generated. Specify -1 to generate all "
+        "possible scenes.")
+
     args = parser.parse_args()
     
     pkl_fname = f'train_{args.part_idx}.pkl'
     pkl_fpath = os.path.join(TRAIN_CAM_DIR, pkl_fname)
-    
+
     with open(pkl_fpath, 'rb') as pkl_f:
         data = pickle5.load(pkl_f)
         num_scenes = data['X'].shape[0]
